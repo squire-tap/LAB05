432ee4d076265e0c81f7dd7b2c919b88c08f5fdc
Luis Henrique <luislopesteo@hotmail.com>
2022-11-03 00:26:10 -0300

    mudancas

--- LAB05_ASYNC/cont_mod_10.vhd
+++ LAB05_ASYNC/cont_mod_10.vhd
@@ -1,26 +1,72 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
+library IEEE;  
+use IEEE.STD_LOGIC_1164.ALL;  
+use IEEE.STD_LOGIC_ARITH.ALL;  
+use IEEE.STD_LOGIC_UNSIGNED.ALL;  
 
-entity cont_mod_10 is
-	port(
-		clk,rst : in  std_logic;
-		count : inout std_logic_vector(3 downto 0)
-		);
-end entity;
+entity cont_mod_10 is  
+    Port ( clock : in std_logic;  
+           --reset : in std_logic;  
+           count : out std_logic_vector(3 downto 0)  
+    );  
+end cont_mod_10;
+  
+architecture rtl of cont_mod_10 is  
+COMPONENT jkff  
+    PORT( clock : in std_logic;  
+          reset : in std_logic;  
+          j     : in std_logic;  
+          k     : in std_logic;  
+          q     : out std_logic      
+    );  
+   END COMPONENT;
+  
+signal temp : std_logic_vector(3 downto 0) := "0000";  
+signal reset : std_logic := '0';
 
-architecture behavioral of cont_mod_10 is 
-begin
-	process(clk,rst)
-		constant modulo : std_logic_vector := "1010";
-	begin
-		if(rst = '1' or count = modulo) then
-			count <= "0000";
-		elsif rising_edge(clk) then
-			count <= count + 1;
-		end if;
-	end process;
-end behavioral;	
-		
+begin  
+d0 : jkff  
+     port map (  
+       reset  => reset,  
+       clock  => clock,  
+       j      => '1',  
+       k      => '1',        
+       q      => temp(3)  
+    );  
 
+d1 : jkff  
+     port map (  
+       reset  => reset,  
+       clock  => temp(3),  
+       j      => '1',  
+       k      => '1',      
+       q      => temp(2)  
+    );  
+
+d2 : jkff  
+     port map (  
+       reset  => reset,  
+       clock  => temp(2),  
+       j      => '1',  
+       k      => '1',  
+       q      => temp(1)  
+    );  
+
+d3 : jkff  
+     port map (  
+       reset  => reset,  
+       clock  => temp(1),  
+       j      => '1',  
+       k      => '1',  
+       q      => temp(0)  
+    );  
+
+--Reseta quando a saida dos ff's chega em 10
+reset <= '1' when (temp(0) = '1' and temp(1) = '1' and temp(2) = '0' and temp(3) = '0')
+    else '0';
+
+count(3) <= temp(0);  
+count(2) <= temp(1);  
+count(1) <= temp(2);  
+count(0) <= temp(3);  
+
+ end rtl;  
